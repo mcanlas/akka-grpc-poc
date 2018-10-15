7cee3079c6c15c40545f393f95bc8437640b9163
Mark Canlas <github@htmlism.com>
2018-10-15 13:46:24 -0400

    add working example

---
+++ src/main/scala/com/htmlism/grpc/PocClient.scala
@@ -0,0 +1,92 @@
+package com.htmlism.grpc
+
+import akka.{ Done, NotUsed }
+import akka.actor.ActorSystem
+import akka.grpc.GrpcClientSettings
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import scala.concurrent.{ ExecutionContextExecutor, Future }
+import scala.concurrent.duration._
+import scala.util.{ Failure, Success }
+
+object PocClient {
+
+  def main(args: Array[String]): Unit = {
+    implicit val sys: ActorSystem = ActorSystem("HelloWorldClient")
+    implicit val mat: ActorMaterializer = ActorMaterializer()
+    implicit val ec: ExecutionContextExecutor = sys.dispatcher
+
+    // Take details how to connect to the service from the config.
+    val clientSettings = GrpcClientSettings.fromConfig("PocSomething")
+    // Create a client-side stub for the service
+    val client: PocService = new PocServiceClient(clientSettings)
+
+    // Run examples for each of the exposed service methods.
+    runSingleRequestReplyExample()
+    runStreamingRequestExample()
+    runStreamingReplyExample()
+    runStreamingRequestReplyExample()
+
+    sys.scheduler.schedule(1.second, 1.second) {
+      runSingleRequestReplyExample()
+    }
+
+    def runSingleRequestReplyExample(): Unit = {
+      sys.log.info("Performing request")
+      val reply = client.sayHello(HelloRequest("Alice"))
+      reply.onComplete {
+        case Success(msg) =>
+          println(s"got single reply: $msg")
+        case Failure(e) =>
+          println(s"Error sayHello: $e")
+      }
+    }
+
+    def runStreamingRequestExample(): Unit = {
+      val requests = List("Alice", "Bob", "Peter").map(HelloRequest.apply)
+      val reply = client.itKeepsTalking(Source(requests))
+      reply.onComplete {
+        case Success(msg) =>
+          println(s"got single reply for streaming requests: $msg")
+        case Failure(e) =>
+          println(s"Error streamingRequest: $e")
+      }
+    }
+
+    def runStreamingReplyExample(): Unit = {
+      val responseStream = client.itKeepsReplying(HelloRequest("Alice"))
+      val done: Future[Done] =
+        responseStream.runForeach(reply => println(s"got streaming reply: ${reply.message}"))
+
+      done.onComplete {
+        case Success(_) =>
+          println("streamingReply done")
+        case Failure(e) =>
+          println(s"Error streamingReply: $e")
+      }
+    }
+
+    def runStreamingRequestReplyExample(): Unit = {
+      val requestStream: Source[HelloRequest, NotUsed] =
+        Source
+          .tick(100.millis, 1.second, "tick")
+          .zipWithIndex
+          .map { case (_, i) => i }
+          .map(i => HelloRequest(s"Alice-$i"))
+          .take(10)
+          .mapMaterializedValue(_ => NotUsed)
+
+      val responseStream: Source[HelloReply, NotUsed] = client.streamHellos(requestStream)
+      val done: Future[Done] =
+        responseStream.runForeach(reply => println(s"got streaming reply: ${reply.message}"))
+
+      done.onComplete {
+        case Success(_) =>
+          println("streamingRequestReply done")
+        case Failure(e) =>
+          println(s"Error streamingRequestReply: $e")
+      }
+    }
+  }
+
+}
